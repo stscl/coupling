9c5a0fd2f31fe2066c3396770dc4a65f417ce894
SpatLyu <lyu.geosocial@gmail.com>
2026-06-16 00:00:01 +0800

    export ccd method

--- src/CCD.cpp
+++ src/CCD.cpp
@@ -7,11 +7,20 @@
 
 // Wrapper function to preform coupling coordination degree analysis
 // [[Rcpp::export(rng = false)]]
-Rcpp::List RcppCCD(const Rcpp::NumericMatrix& mat,
+Rcpp::DataFrame RcppCCD(const Rcpp::NumericMatrix& mat,
                    const Rcpp::NumericVector& weight,
                    const std::string& method = "standard")
 {   
     std::vector<std::vector<double>> mat_std = coupling::convert::mat_r2std(mat, true);
     std::vector<double> wt_std = Rcpp::as<std::vector<double>>(weight);
+
+    // Call ccd function
+    std::vector<std::vector<double>> res = coupling::ccd::ccd(
+        mat_std, wt_std, method
+    );
     
+    return Rcpp::DataFrame::create( 
+        Rcpp::Named("C") = res[0], 
+        Rcpp::Named("D") = res[1]
+    );
 }
